aa2a8a7ea7cffb91372013a9998682fd7f193dfd
mentalmystery94 <timkwaijtaal@gmail.com>
2025-01-07 20:31:46 +0100

    5 seconds back homepage

--- src/components/ConfirmationPage.js
+++ src/components/ConfirmationPage.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './ConfirmationPage.css';
 
 const ConfirmationPage = () => {
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href = "/"; // Redirect to the homepage
+    }, 5000); // 5 seconds
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []);
+
   return (
     <div className="confirmation-page">
       <h2>Order Confirmation</h2>
